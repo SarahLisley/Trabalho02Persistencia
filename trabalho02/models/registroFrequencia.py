0b852c43aec3aef8df88ef5f0683e648443a4e08
SarahLisley <109000935+SarahLisley@users.noreply.github.com>
2025-06-11 10:56:06 -0300

    Delete trabalho02 directory

--- trabalho02/models/registroFrequencia.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-from datetime import datetime
-
-class RegistroFrequencia(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    viagem_id: int = Field(foreign_key="viagem.id")
-    aluno_id: int   # se você quiser FK real, precisa importar e definir Aluno
-    data_hora_embarque: datetime
-    tipo_registro: str  # Ex: "Embarque" ou "Desembarque"
